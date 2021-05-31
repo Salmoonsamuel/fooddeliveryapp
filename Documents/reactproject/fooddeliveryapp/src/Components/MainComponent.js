47fd1b807481f3c0ec523e021be8441139adcdc6
Salmoonsamuel <salmoonsamuel08@gmail.co>
2021-05-30 23:32:17 -0400

    Add Header.js file

---
+++ Documents/reactproject/fooddeliveryapp/src/Components/MainComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+
+class Main extends component {
+
+    render(){
+        return(
+            <div>
+               <Header />
+            </div>
+        );
+    }
+}
+export default Main;
